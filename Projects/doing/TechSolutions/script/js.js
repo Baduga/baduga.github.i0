a912077ee1643ecd37b45d919a8e40b24181189a
onion <69755854+Baduga@users.noreply.github.com>
2024-08-31 11:10:13 +0300

    .

--- Projects/doing/TechSolutions/script/js.js
+++ Projects/doing/TechSolutions/script/js.js
@@ -1,5 +1,8 @@
 window.addEventListener("scroll", function(paallax){
         document.querySelector('.block1').style.backgroundSize = (this.scrollY/25 +100)+"%";
-        document.querySelector('.bright-wrap').style.width = (this.scrollY/10 +100)+"%";
+        if (this.scrollY<300) {
+                document.querySelector('.bright-wrap').style.width = (this.scrollY/10 +100)+"%";
+
+        }
 
 })
